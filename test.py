b3519106328a0928576f07887267db4d50ba2423
sshnote <springssh0504@naver.com>
2024-03-31 21:12:50 +0900

    코드 수정

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ for i in range(60 * 17+1):
             print(f'BUS A | BUS C 동시 운행\t : \t{(60 * 6 + i) // 60}시 {(60 * 6 + i) % 60}분 ')
         if i % stepC == 0 and i % stepB == 0:
             print(f'BUS B | BUS C 동시 운행\t : \t{(60 * 6 + i) // 60}시 {(60 * 6 + i) % 60}분 ')
-
+# 주석 추가
     if i % stepA == 0 and i % stepB == 0:
         print(f'BUS A | BUS B 동시 운행\t : \t{(60 * 6 + i) // 60}시 {(60 * 6 + i) % 60}분 ')
