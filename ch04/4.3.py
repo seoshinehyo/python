ccd5559110f237f986a4b21cfab76b4cb59c0a3d
sshnote <springssh0504@naver.com>
2024-04-05 10:01:15 +0900

    if-else문 예제

---
+++ ch04/4.3.py
@@ -0,0 +1,25 @@
+# (1)
+num = int(input("정수를 입력하세요: "))
+
+if (num < 0):
+    print('음수입니다.')
+else:
+    print('음수가 아닙니다.')
+
+#(2)
+game_score = int(input('게임 점수를 입력하세요: '))
+
+if (game_score >= 10000):
+    print('고수입니다.')
+else:
+    print('입문자입니다.')
+
+#(3)
+num1 = int(input("정수1을 입력하세요: "))
+num2 = int(input("정수2를 입력하세요: "))
+
+if (num1 == num2):
+    print('두 값이 일치합니다.')    
+else:
+    print('두 값이 일치하지 않습니다.')
+
