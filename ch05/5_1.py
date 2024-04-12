b69f08a66292cb4d747152ecd22c7eaa6a73ad5f
sshnote <springssh0504@naver.com>
2024-04-12 09:57:58 +0900

    5장 예제 정리

---
+++ ch05/5_1.py
@@ -0,0 +1,12 @@
+# for i in range(100):
+#     print('파이썬 주식회사의 방문을 환영합니다!!')
+#     if (i == 50):
+#         break
+
+# for i in range(100):
+#     if (i < 50):
+#         print('환영합니다!')
+
+for i in range(100):
+    if(i % 2 == 0):
+        print('환영합니다!')
