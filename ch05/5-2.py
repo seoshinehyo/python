565022143a6bfc4e257864dbec68f374b662d9a6
sshnote <springssh0504@naver.com>
2024-04-12 10:55:37 +0900

    5-2 예제

--- ch05/5-2.py
+++ ch05/5-2.py
@@ -1,3 +1,10 @@
-bts = ['V', 'J-Hope', 'RM', 'Jungkook', 'Jin', 'Jimin', 'Suga']
-for i in bts:
-    print(i)
+# bts = ['V', 'J-Hope', 'RM', 'Jungkook', 'Jin', 'Jimin', 'Suga']
+# for i in bts:
+#     print(i)
+
+n = int(input('정수를 입력하시오: '))
+fact = 1
+for i in range(n+1, 1, -1):
+    fact = fact * i
+
+print(n, '!은', fact, "이다.")
