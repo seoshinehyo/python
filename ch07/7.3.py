ddf2bbea93bac058ec70965e5f72516ad5b67aca
sshnote <springssh0504@naver.com>
2024-05-10 10:20:25 +0900

    zip 실습

---
+++ ch07/7.3.py
@@ -0,0 +1,7 @@
+s = 'Python is fun'
+
+print(s[10:13])
+
+print(s[:6])
+
+print(s[7:9])
---
+++ ch07/ex7.6.py
@@ -0,0 +1,6 @@
+# (1)
+name = ['한국', '미국', '중국', '필리핀']
+distance = [100.2, 9834, 9597, 300]
+
+city = list(zip(name, distance))
+print('city = ', city)
