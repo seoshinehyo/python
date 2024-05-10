57c262635f56faf07383a96cd9f71827b15fb10a
sshnote <springssh0504@naver.com>
2024-05-10 10:22:40 +0900

    zip 실습2

--- ch07/ex7.6.py
+++ ch07/ex7.6.py
@@ -4,3 +4,8 @@ distance = [100.2, 9834, 9597, 300]
 
 city = list(zip(name, distance))
 print('city = ', city)
+
+# (2)
+sudo = ['서울', '워싱턴', '베이징', '마닐라']
+city_info = list(zip(name, sudo, distance))
+print('city_info = ', city_info)
