d712dbbe2db1a44ef6f6b60f443831cda72a313d
sshnote <springssh0504@naver.com>
2024-05-13 21:54:21 +0900

    ex8.1 수정

--- ch08/8.1.py
+++
@@ -1,7 +0,0 @@
-phone_book = {'홍길동': '010-1234-5678'}
-phone_book = {'강감찬': '010-1234-5678'}
-phone_book = {'이순신': '010-1234-5678'}
-
-
-for name in phone_book.keys():  # 피피티 말고 이렇게 하기
-    print(name, ':', phone_book[name])
---
+++ ch08/ex8.1.py
@@ -0,0 +1,16 @@
+phone_book = {'홍길동': '010-1234-5678',
+              '강감찬': '010-1234-5678',
+              '이순신': '010-1234-5678'}
+
+for name in phone_book.keys():
+    print(name, ':', phone_book[name])
+
+for name, number in phone_book.items():
+    print(name, ':', number)
+
+contact = {'성': '홍',
+           '이름': '길동',
+           '직장': '삼성'}
+
+for key in contact.keys():
+    print(key, ':', contact[key])
