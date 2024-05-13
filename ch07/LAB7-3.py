e2180fa05ffbef36b37a075e6017875b0ebfdc53
sshnote <springssh0504@naver.com>
2024-05-13 20:30:31 +0900

    7.4~7.6

---
+++ ch07/LAB7-3.py
@@ -0,0 +1,15 @@
+population = ['Seoul', 9765, 'Busan', 3441, 'Incheon', 2954]
+
+# 1)
+print(population[1])
+
+# 2)
+print(population[-1])
+
+# 3)
+print(population[::2])
+
+# 4)
+print(population[1::2])
+sum = sum(population[1::2])
+print('인구의 합:', sum)
---
+++ ch07/ex7.3.py
@@ -0,0 +1,16 @@
+s = 'Python is fun'
+
+# (1)
+print(s[0])
+
+# (2)
+print(s[10:13])
+
+# (3)
+print(s[:6])
+
+# (4)
+print(s[7:9])
+
+print(sorted('FADEWAY', reverse=True))
+print(s[-1:-3:-1])
