93072c3221df31465234a7979130e806cd634fca
sshnote <springssh0504@naver.com>
2024-05-16 19:34:01 +0900

    과제 수정

--- week10/7.7.py
+++ week10/7.7.py
@@ -3,9 +3,12 @@ fruit_list = ['banana', 'orange', 'kiwi', 'apple', 'melon']
 
 # (1) 가장 길이가 긴 문자열 출력하고, 리스트에서 삭제
 max_length = len(max(fruit_list))  # 가장 긴 문자열의 길이 구하기
-longest_fruit = [fruit for fruit in fruit_list if len(fruit) == max_length]   # 가장 긴 문자열을 가진 과일들을 원소로 갖는 리스트
 
-print(f'가장 길이가 긴 문자열 :, {longest_fruit[0]}, {longest_fruit[1]}')    # 가장 길이가 긴 과일 출력
+# 가장 긴 문자열을 가진 과일들을 원소로 갖는 리스트
+longest_fruit = [fruit for fruit in fruit_list if len(fruit) == max_length]   
+
+# 가장 길이가 긴 과일 출력
+print(f'가장 길이가 긴 문자열 :, {longest_fruit[0]}, {longest_fruit[1]}')    
 
 # 기존 리스트에서 가장 긴 문자열을 가진 과일들 제거
 for fruit in longest_fruit:
