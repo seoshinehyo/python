7c2991a946ac16dcb1b6907df6fb554dc2192c6f
sshnote <springssh0504@naver.com>
2024-05-24 10:09:50 +0900

    10.1 ~ 10.4

---
+++ ch10/ex10.1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array(range(1,11))
+b = np.array(range(10, 101, 10))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
---
+++ ch10/ex10.3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+score = [69, 34, 55, 79, 90, 57, 92, 59, 69, 77]
+scores = np.array(score)
+
+
+print(scores[scores >= 60])
