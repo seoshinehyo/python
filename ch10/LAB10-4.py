877f51a11c4caccbfe833dfb27273d7dd6eb44d4
sshnote <springssh0504@naver.com>
2024-05-24 10:46:20 +0900

    10

---
+++ ch10/LAB10-4.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+x = np.array([[1.83, 1.76, 1.69, 1.86, 1.77, 1.73],
+              [86.0, 74.0, 59.0, 95.0, 80.0, 68.0]])
+
+y = x[0:2, 1:3]
+z = x[0:2][1:3]
+
+print(f'x shape : {x.shape}')
+print(f'y shape : {y.shape}')
+print(f'z shape : {z.shape}')
+print(f'z values : {z}')
+print('BMI data')
+print(x[1] / (x[0]**2))
