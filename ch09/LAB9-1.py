7590be96957ac12f1d4296298d5344d738ef0ba5
sshnote <springssh0504@naver.com>
2024-06-12 22:58:36 +0900

    9장 코드

--- ch09/LAB9-1.py
+++ ch09/LAB9-1.py
@@ -1,10 +1,17 @@
 s = 'This movie was terrible! The acting was poor and the story was boring.'
 
-print(f's={s}')
+# print(f's={s}')
+# print(f'글자수={len(s)}')
+# words = s.split()
+# print(f'단어들의 리스트={words}')
+# print(f'단어 수={len(words)}')
+# print(f'평균 단어 길이={sum(len(word) for word in words) / len(words)}')
+
+# print(s.count('#'))
+
 print(f'글자수={len(s)}')
-words = s.split()
+words = list(s.split())
 print(f'단어들의 리스트={words}')
 print(f'단어 수={len(words)}')
-print(f'평균 단어 길이={sum(len(word) for word in words) / len(words)}')
-
-print(s.count('#'))
+avg_len = sum(len(word) for word in words) / len(words)
+print(f'평균 단어 길이={avg_len}')
--- ch09/LAB9-2.py
+++ ch09/LAB9-2.py
@@ -1,8 +1,16 @@
 stop_words = ['is', 'and', 'the', 'a', 'an', 'in', 'of', 'to']
 
 s = 'This is an example of removing stop words from a string.'
+# words = s.split()
+# filtered_sentence = [word for word in words if word.lower() not in stop_words]
+
+# print(f'입력={s}')
+# print(f'출력={" ".join(filtered_sentence)}')
+
 words = s.split()
-filtered_sentence = [word for word in words if word.lower() not in stop_words]
+filtered_words = [word for word in words if word not in stop_words]
 
 print(f'입력={s}')
-print(f'출력={" ".join(filtered_sentence)}')
+print(f'출력={" ".join(filtered_words)}')
+
+
